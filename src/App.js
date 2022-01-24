de083289185c0f4cc28a5b3858dfa353d9f1ff0e
louis <louisasare@hotmail.com>
2022-01-24 00:13:03 +0000

    add dynamic content

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'; /// A statement that imports "Person" from the Person.js file
 
-class App extends Component {
+class App extends Component { /// person tag in this funtion is a self closing tag
   render() {
     return (
       <div className='App'> 
       
-      <h1> Hi, im a react app</h1>
+      <h1> Hi, im a react app</h1> 
 
-      <Person/> ////self closing tag that is imported from the person.js file
-
+      <Person/>  
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react'; // have to import react to use any features
 
-const person = () => {
-return <p> i'm a person! </p>
+const person = () => { /// function that returns javascriptfx 
+return <p> i'm a person! and i am {Math.floor(Math.random() * 30)} </p> // added dynamic content that generates random age
 }
 
 export default person; /// Makes person the default export in the file.
