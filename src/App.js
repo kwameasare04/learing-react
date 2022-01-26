060ae189d4a764fff1033b4167a66c528b751c76
louis <louisasare@hotmail.com>
2022-01-26 17:12:42 +0000

    react_hooks

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './App.css';
 import Person from './Person/Person'; /// A statement that imports "Person" from the Person.js file
 
-class App extends Component { /// person tag in this funtion is a self closing tag
-  state = {
+const app = props =>{ 
+  const [personState, personSetState] =  useState({
     people: [
-      {name: 'Kevin', age:'36'},  
-      {name: 'Max', age:'28'},
-      {name: 'Rodney', age:'19'}
-    ]
-  } /// state object accepts any type of data 
-
+          {name: 'Kevin', age:'36'},  
+          {name: 'Max', age:'28'},
+          {name: 'Rodney', age:'19'}
+        ]});
 
   switchNameHandler = () =>{ ////event handler function 
     // console.log("switch name");
-    this.setState({
+    personSetState({
       people: [
         {name: 'Kevin', age:'37'},  
         {name: 'Maxim', age:'29'},
@@ -23,7 +21,7 @@ class App extends Component { /// person tag in this funtion is a self closing t
     })
 
   }
-  render() {
+
 
     return (/// properties added to the person tags
 <div className='App'> 
@@ -32,14 +30,35 @@ class App extends Component { /// person tag in this funtion is a self closing t
         {/* onClick added that calls the event handler function when button is clicked */}
       <button onClick={this.switchNameHandler}>click me!</button>
 
-      <Person name = {this.state.people[0].name} age={this.state.people[0].age}> My hobbies are : skiing and food </Person> 
-      <Person name={this.state.people[1].name} age={this.state.people[1].age}> </Person>
-      <Person name={this.state.people[2].name}age={this.state.people[2].age}> My hobbies are : Saxaphone and Hockey </Person>
+      <Person name = {personState.people[0].name} age={personState.people[0].age}> My hobbies are : skiing and food </Person> 
+      <Person name={personState.people[1].name} age={personState.people[1].age}> </Person>
+      <Person name={personState.people[2].name}age={personState.people[2].age}> My hobbies are : Saxaphone and Hockey </Person>
       
       </div>
     
     );
   }
-}
+
 /// "this" in the name and age attribute refers to this class. state the object in the class and the people array in the object.
-export default App;
+export default app;
+// const app = props => { /// person tag in this funtion is a self closing tag
+//   state = {
+//     people: [
+//       {name: 'Kevin', age:'36'},  
+//       {name: 'Max', age:'28'},
+//       {name: 'Rodney', age:'19'}
+//     ]
+//   } /// state object accepts any type of data 
+
+
+//   switchNameHandler = () =>{ ////event handler function 
+//     // console.log("switch name");
+//     this.setState({
+//       people: [
+//         {name: 'Kevin', age:'37'},  
+//         {name: 'Maxim', age:'29'},
+//         {name: 'Rodney', age:'20'}
+//       ]
+//     })
+
+//   }
