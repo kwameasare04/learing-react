210d4675655460b8d850bf60ef14123a0a02c849
louis <louisasare@hotmail.com>
2022-01-27 01:40:28 +0000

    react hooks final

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'; /// A statement that imports "Person" from the Person.js file
 
