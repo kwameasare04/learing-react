14300bd13c23370f1e1d082690494f8d9941c5e7
louis <louisasare@hotmail.com>
2022-01-31 23:31:28 +0000

    adding style sheet

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,11 @@
 import React from 'react'; // have to import react to use any features
-
+import "./Person.css" /// have to import css file to use it
 const person = (props) => { /// function returns a paragraph with the property name and age in statement
 return (
 /* dynamic content that returns properties name and age */
-<div>
+<div className="Person"> {/* giving div class name so it can be targeted in css file}
          {/*onClick being assigned to property the person element*/}
-     <p onClick={props.click}> i'm a {props.name} and i am {props.age} </p>  
+     <p onClick={props.click}> hello i'm {props.name} and i am {props.age} </p>  
      {/* onChange has reference to property change with is a method with a set state function  */}
      {/* assaigning props.name to value makes the name visible from the start */}
      < input type="text" onChange={props.change} value={props.name}/>
