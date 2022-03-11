0d821596ae76857097ce34f980b413ec46c6efac
louis <louisasare@hotmail.com>
2022-03-11 16:19:05 +0000

    higher_order_component

--- src/components/People/Person/Person.js
+++ src/components/People/Person/Person.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'; // have to import react to use any features
 import classes from "./Person.css" /// have to import css file to use it
-// import Radium from 'radium';
+import Aux from '../../../hoc/Aux'
 class Person extends Component  { /// function returns a paragraph with the property name and age in statement
     // const random = Math.random();
     // if(random > 0.7) {
@@ -8,21 +8,23 @@ class Person extends Component  { /// function returns a paragraph with the prop
     // }
 
    render(){
-      console.log('[person.js] rendering...')
-        return (
-        /* dynamic content that returns properties name and age */
-        <div className={classes.Person}> {/* giving div class name so it can be targeted in css file}
-         {/*onClick being assigned to property the person element*/}
-       <p onClick={this.props.click}> hello i'm {this.props.name} and i am {this.props.age} </p>  
-        {/* onChange has reference to property change with is a method with a set state function  */}
-        {/* assaigning props.name to value makes the name visible from the start */}
-        < input type="text" onChange={this.props.changed} value={this.props.name}/>
-        <p>{this.props.children}</p>
-        </div> ) 
-       }
-    }
+    console.log('[person.js] rendering...')
+    return(
+     <Aux>
+
+       <p key={"item1"} onClick={this.props.click}> hello i'm {this.props.name} and i am {this.props.age} years old!</p>
+        <p key={"item2"}>{this.props.children}</p>      
+        <input key={"item3"} type="text" onChange={this.props.changed} value={this.props.name}/>
+     </Aux>
+        );     
+           }
+}
+          
+    
 // props.children Allows for content to be added from outside person.js file
 export default Person; /// Makes person the default export in the file.
 
 
- 
+        //  /* dynamic content that returns properties name and age */
+        //  <div className={classes.Person}> {/* giving div class name so it can be targeted in css file}
+        //  {/*onClick being assigned to property the person element*/}
---
+++ src/hoc/Aux.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const aux = props => props.children;
+
+export default aux;
+
+/// this is an emtpy wrapper that uses the children property, and children refers to the content in the tag it is used in.
+/// and outputs that content
